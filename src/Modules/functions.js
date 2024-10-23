f4fc60bb1e8fa034c47d14481dae3373fa71afb9
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-10-23 17:58:13 +0200

    Improved cells sorting algorithm

--- src/Modules/functions.js
+++ src/Modules/functions.js
@@ -34,7 +34,7 @@ export function populateBoard(target) {
 
 
 
-export function placeShip(element, location) {
+export function placeShip(element, location, cells) {
   element.onmousedown = dragShip;
   let shipAngle = 0;
 
@@ -54,7 +54,6 @@ export function placeShip(element, location) {
 
     
     function rotateShip(e) {
-      console.log(e);
       console.log(shipRect);
       if (e.key == 'q') {
         test.style.transform = `rotate(${shipAngle - 45}deg)`;
@@ -76,12 +75,105 @@ export function placeShip(element, location) {
       nodes.main.prepend(nodes.board)
       document.onmouseup = null;
       document.onmousemove = null;
-      document.removeEventListener('onkeypress', rotateShip)
+      document.removeEventListener('onkeypress', rotateShip);
+      element.style.opacity = '0.5';
+      element.onmousedown = null;
     }
     
     function isOverlapping(rect1, rect2) {
-      return !(rect1.right < rect2.left || rect1.top > rect2.bottom || rect1.left > rect2.right || rect1.bottom < rect2.top || rect1.bottom > rect2.top && rect1.top < rect2.top);
+      return !(rect1.right < rect2.left || 
+        rect1.top > rect2.bottom || 
+        rect1.left > rect2.right || 
+        rect1.bottom < rect2.top || 
+        rect1.bottom > rect2.top && 
+        rect1.top < rect2.top);
     }
   }
 }
 
+export function cellsCalc() {
+  const cells = Array.from(document.querySelectorAll('.cell'));
+  const cellsData = [
+    {Quadrant: 0,
+      top: null,
+      bottom: null,
+      left: null,
+      right: null,
+      rows: [],
+      columns: [],
+    },
+    {Quadrant: 1,
+      top: null,
+      bottom: null,
+      left: null,
+      right: null,
+      rows: [],
+      columns: [],
+    },
+    {Quadrant: 2,
+      top: null,
+      bottom: null,
+      left: null,
+      right: null,
+      rows: [],
+      columns: [],
+    },
+    {Quadrant: 3,
+      top: null,
+      bottom: null,
+      left: null,
+      right: null,
+      rows: [],
+      columns: [],
+    },
+  ];
+
+  for (let i = 0; i < cells.length; i++) {
+    const cell = cells[i];
+    const cellSquare = cell.getBoundingClientRect();
+    const cellRow = cell.dataset.row;
+    const cellColumn = cell.dataset.column;
+    let infoObj = {
+      id: i,
+      row: cellRow,
+      column: cellColumn,
+      node: cell,
+      left: cellSquare.left,
+      right: cellSquare.right,
+      top: cellSquare.top,
+      bottom: cellSquare.bottom
+    }
+    const quadrant = getQuadrant(infoObj);
+    let arraySlotRow = cellsData[quadrant].rows.find((item) => item.id == cellRow);
+    if (!arraySlotRow) {
+      const newArrLength = cellsData[quadrant].rows.push({id: cellRow, cells: [], top: cellSquare.top, bottom: cellSquare.bottom, left: cellSquare.left});
+      arraySlotRow = cellsData[quadrant].rows[newArrLength - 1];
+    };
+    let arraySlotColumn = cellsData[quadrant].columns.find((item) => item.id == cellColumn);
+    if (!arraySlotColumn) {
+      const newArrLength = cellsData[quadrant].columns.push({id: cellColumn, cells: [], left: cellSquare.left, right: cellSquare.right, top: cellSquare.top});
+      arraySlotColumn = cellsData[quadrant].columns[newArrLength - 1];
+    };
+    
+    if (cellsData[quadrant].rows.length === 5 && cellsData[quadrant].columns.length === 5) {
+      cellsData[quadrant].columns.forEach((column) => column.bottom = cellSquare.bottom);
+      cellsData[quadrant].rows.forEach((row) => row.right = cellSquare.right);
+      cellsData[quadrant].bottom = cellSquare.bottom;
+      cellsData[quadrant].right = cellSquare.right;
+      cellsData[quadrant].top = cellsData[quadrant].columns[0].top;
+      cellsData[quadrant].left = cellsData[quadrant].columns[0].left;
+    }
+    arraySlotRow.cells.push(infoObj);
+    arraySlotColumn.cells.push(infoObj);
+  }
+
+  function getQuadrant(cell) {
+    const unit = Math.sqrt(cells.length) / 2;
+    if (cell.row < unit && cell.column < unit) return 0;
+    else if (cell.row < unit && cell.column >= unit) return 1;
+    else if (cell.row >= unit && cell.column < unit) return 2;
+    else return 3;
+  }
+
+  return cellsData;
+}
--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
 import "./Styles/styles.css";
 import * as nodes from "./Modules/selectors";
-import { createElement, populateBoard, placeShip, getImages, } from "./Modules/functions";
+import { createElement, populateBoard, placeShip, getImages, cellsCalc, } from "./Modules/functions";
 
 let cells; 
-let cellData;
+let quadrants;
 const images = getImages();
 
 document.addEventListener('DOMContentLoaded', (e) => {
   populateBoard(nodes.board);
-  cellsCalc()
-  const ship = document.querySelector('img.ship');
-  placeShip(ship, images['car-hor.png']);
+  cells = cellsCalc();
+  const ship = document.querySelectorAll('img.ship');
+  ship.forEach((ship) => {
+    const shipType = ship.dataset.type;
+    placeShip(ship, images[`${shipType}-hor.png`], cells)
+  });
+  console.log(cells);
 });
 
 
-function cellsCalc() {
-  cells = Array.from(document.querySelectorAll('.cell'));
-  const arr = [];
-  for (let i = 0; i < cells.length; i++) {
-    const cellSquare = cells[i].getBoundingClientRect();
-    arr[i] = {
-      cellCount: i,
-      left: cellSquare.left,
-      right: cellSquare.right,
-      top: cellSquare.top,
-      bottom: cellSquare.bottom
-    }
-  }
 
-  cellData = arr;
-}
 
 
