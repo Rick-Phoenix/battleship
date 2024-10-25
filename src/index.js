dbd0a2f4064feea0ff69376bc9adaff5f70bd365
Rick-Phoenix <180959305+Rick-Phoenix@users.noreply.github.com>
2024-10-25 16:25:21 +0200

    Added initial game mechanics

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import "./Styles/styles.css";
 import * as nodes from "./Modules/selectors";
-import { createElement, populateBoard, placeShip, getImages, cellsCalc, isOverlapping, } from "./Modules/functions";
+import { createElement, populateBoard, placeShip, getImages, cellsCalc, isOverlapping, computerShipPlacement, } from "./Modules/functions";
+import { Ship } from "./Modules/classes";
 
 let cells; 
+let computerCells;
 
 document.addEventListener('DOMContentLoaded', (e) => {
-  populateBoard(nodes.board);
-  cells = cellsCalc();
+  populateBoard(nodes.board, 'player');
+  populateBoard(nodes.computerBoard, 'computer');
+  cells = cellsCalc('player');
+  computerCells = cellsCalc('computer');
   const ship = document.querySelectorAll('img.ship');
   ship.forEach((ship) => {
     const shipType = ship.dataset.type;
-    placeShip(ship, shipType, getImages()[`${shipType}-hor.png`], cells)
+    placeShip(ship, shipType, getImages()[`${shipType}-hor.png`], cells);
+    computerShipPlacement(shipType, computerCells)
   });
-  console.log(cells);
+
 
 });
 
